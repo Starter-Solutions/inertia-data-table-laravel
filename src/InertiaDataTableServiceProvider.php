c0371930721d7c23959e5abb0a1f72dd691331bc
HoffmannLuca <luca.hoffmann1000@gmail.com>
2026-03-06 01:00:41 +0100

    Add Inertia data table config sharing functionality in service provider

--- src/InertiaDataTableServiceProvider.php
+++ src/InertiaDataTableServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace StarterSolutions\InertiaDataTable;
 
-use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
+use Inertia\Inertia;
 use StarterSolutions\InertiaDataTable\Macros\DataTableMacros;
 
 class InertiaDataTableServiceProvider extends ServiceProvider
@@ -15,6 +15,21 @@ class InertiaDataTableServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        Inertia::share('inertiaDataTable', function () {
+            return [
+                'queryParams' => [
+                    'perPage' => config('inertia-data-table.per_page_param', 'per_page'),
+                    'sortBy' => config('inertia-data-table.sort_by_param', 'sort_by'),
+                    'descending' => config('inertia-data-table.descending_param', 'descending'),
+                    'pageName' => config('inertia-data-table.page_name_param', 'page'),
+                ],
+                'defaults' => [
+                    'perPage' => config('inertia-data-table.default_per_page', 15),
+                    'sortBy' => config('inertia-data-table.default_sort_by', 'id'),
+                ],
+            ];
+        });
+
         // publish config
         $this->publishes([
             __DIR__.'/../config/inertia-data-table.php' => $this->app->configPath('inertia-data-table.php'),
